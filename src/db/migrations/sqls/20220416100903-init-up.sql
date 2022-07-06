7127cbdf1ccbde2da6c94f6be8fd0d0553ef1e0a
murat <murat.byazrov@yandex.ru>
2022-07-06 23:29:53 +0300

    account interests

--- src/db/migrations/sqls/20220416100903-init-up.sql
+++ src/db/migrations/sqls/20220416100903-init-up.sql
@@ -36,13 +36,56 @@ CREATE TABLE IF NOT EXISTS accounts (
     ,modify_datetime TIMESTAMP WITH TIME ZONE
 );
 
+CREATE TABLE IF NOT EXISTS interest_categories (
+     interest_category_id SMALLSERIAL NOT NULL UNIQUE
+    ,interest_category_value_ru TEXT NOT NULL UNIQUE
+    ,interest_category_value_en TEXT NOT NULL UNIQUE
+);
+
 CREATE TABLE IF NOT EXISTS interests (
-     interest_id SMALLINT NOT NULL UNIQUE
-    ,interest_text TEXT NOT NULL UNIQUE
-    ,interest_category TEXT NOT NULL
+     interest_id SMALLSERIAL NOT NULL UNIQUE
+    ,interest_value_ru TEXT NOT NULL UNIQUE
+    ,interest_value_en TEXT NOT NULL UNIQUE
+    ,interest_category_id SMALLINT REFERENCES interest_categories (interest_category_id)
 );
 
 CREATE TABLE IF NOT EXISTS interests_accounts (
      interest_id SMALLINT NOT NULL REFERENCES interests(interest_id)
     ,account_id TEXT NOT NULL REFERENCES accounts(account_id)
 );
+
+INSERT INTO interest_categories (
+     interest_category_value_ru
+    ,interest_category_value_en
+) VALUES
+     ('Еда', 'Eat')
+    ,('Музыка', 'Music')
+    ,('Фильмы', 'Movies')
+    ,('Мода', 'Casual')
+    ,('Спорт', 'Sport')
+    ,('Автомобили', 'Cars')
+    ,('Технологии', 'Technologies')
+    ,('Образование', 'Education');
+
+
+INSERT INTO interests AS ic (
+     interest_category_id
+    ,interest_value_ru
+    ,interest_value_en
+) VALUES
+     (1, 'Здоровое питание', 'healthy eating')
+    ,(1, 'ВкуссВилл', 'Vkusvill')
+    ,(2, 'Рок', 'Rock')
+    ,(2, 'Метал', 'Metal')
+    ,(3, 'Марвел', 'Marvel')
+    ,(3, 'DC', 'DC')
+    ,(4, 'Zara', 'Zara')
+    ,(4, 'Ногти', 'Nails')
+    ,(5, 'Велоспорт', 'Cycling')
+    ,(5, 'Лыжи', 'Skis')
+    ,(6, 'ТопГир', 'TopGir')
+    ,(6, 'Мерседес', 'Mercedes')
+    ,(7, 'Курсы программирвания', 'Programming courses')
+    ,(7, 'JavaScript', 'JavaScript')
+    ,(8, 'Школа', 'School')
+    ,(8, 'Английский язык', 'English');
