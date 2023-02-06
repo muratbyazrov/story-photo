bf9b4191730edf40326f51ee9050ee5a7553ea58
murat <murat.byazrov@yandex.ru>
2023-02-06 11:10:49 +0800

    new story version

--- config.js
+++ config.js
@@ -20,12 +20,12 @@ module.exports = {
         port: DB_PORT || 5432,
     },
     http: {
-        host: '192.168.1.10',
+        host: '192.168.1.13',
         port: HTTP_PORT || 3001,
         path: HTTP_PATH || '/story-account-api/v1',
     },
     ws: {
-        host: '192.168.1.10',
+        host: '192.168.1.13',
         port: WS_PORT || 9001,
     },
     token: {
