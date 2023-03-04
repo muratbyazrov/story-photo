26de61c006fd75b64c27ea4c17a5808b4f3b12f6
murat <murat.byazrov@yandex.ru>
2023-03-04 23:12:15 +0800

    first commit

--- src/db/migrations/sqls/20220416100903-init-up.sql
+++ src/db/migrations/sqls/20220416100903-init-up.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS photos (
      photo_id       BIGSERIAL               PRIMARY KEY
     ,path_1         TEXT                    NOT NULL
-    ,path_2         TEXT                    NOT NULL
+    ,path_2         TEXT
     ,replica_1      BOOLEAN                 NOT NULL
     ,replica_2      BOOLEAN
     ,create_dttm    TIMESTAMP DEFAULT NOW() NOT NULL
